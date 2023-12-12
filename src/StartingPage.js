92f1574498c6f7714003a0ecaa5d9c6ca2ec3fa8
illiamartynov <ilyamartynov989@gmail.com>
2023-12-12 03:16:57 +0100

    chat model header information done

--- src/StartingPage.js
+++ src/StartingPage.js
@@ -1,20 +1,10 @@
 import React from "react";
 import styles from "./StartingPage.module.css";
-import UpWrapper from "./Wrapper/UpWrapper.js";
-import Wrapper from "./Wrapper/Wrapper.js"
-import MainPageHeader from "./Components/Header/MainPageHeader.js";
 import MainPageChats from "./Components/Body/Chats/MainPageChats.js";
-import Header from "./Components/Footer/Footer.js";
 const StartingPage = () => {
   return (
     <div>
-      <UpWrapper>
-        <MainPageHeader />
-      </UpWrapper>
       <MainPageChats />
-      <Wrapper>
-        <Header/>
-      </Wrapper>
     </div>
   );
 };
